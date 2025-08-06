8aa1439751ef2993aceba44874c67d4924924f61
Reysian <71834018+Reysian@users.noreply.github.com>
2025-08-05 21:16:42 -0500

    Add functionality that allows users to add todos using the form.

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import TodoForm from './TodoForm';
 import { useState } from 'react';
 
 function App() {
-  const [newTodo, setNewTodo] = useState('sample');
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = (title) => {
+    const newTodo = { id: Date.now(), title };
+    setTodoList([...todoList, newTodo]);
+  };
 
   return (
     <div>
       <h1>To-do list</h1>
-      <TodoForm />
-      <p>{newTodo}</p>
-      <TodoList />
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
