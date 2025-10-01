dac1bbe96274f3206601467486129e3eda6b8807
Reysian <71834018+Reysian@users.noreply.github.com>
2025-09-30 19:29:11 -0500

    Add About and Not Found pages. Refactor project to allow multiple pages. Add pagination to TodoList.

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+function About() {
+  return (
+    <div>
+      <p>
+        This multi-page to-do list application was created by Ethan Norrid, and
+        has been last updated on September 30, 2025.
+      </p>
+      <p>
+        Navigating to the Home page, you will find a field to enter new to-do
+        items, and below, options to filter and sort items in the list.
+      </p>
+      <p>
+        Items added to the to-do list will be saved and accessible between visits, and
+        they may be removed by selecting the check box next to the item.
+      </p>
+    </div>
+  );
+}
+
+export default About;
